e7fd2d02da19afb08bd9432a578d918d413153f0
SBViside <ohvatuki@gmail.com>
2023-03-05 21:12:27 +0300

    nothing

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { authController } from "./controllers/authController.js";
 import { body, check } from "express-validator";
+import { authController } from "./controllers/authController.js";
 import { authOnlyMiddleware } from "./middlewares/authOnlyMiddleware.js";
 import userController from "./controllers/userController.js";
 import imageController from "./controllers/imageController.js";
@@ -8,35 +8,35 @@ import { httpFileExists, httpFileIsImage } from "./modules/validation.js";
 
 const router = Router();
 
-router.get('/images/tags', imageController.tags); // IMAGE TAGS BY SIGNATURE
+router.get('/images/tags', imageController.tags); // REVIEW IMAGE TAGS BY SIGNATURE
 
-// router.get('/images', null); // GET LATEST IMAGES BY PAGE 
-// router.get('/images/random', null); // GET RANDOM IMAGE (FIX)
-// router.get('/images/search', null); // SEARCH IMAGES BY TAGS OR NAME
-// router.get('/images/:id', null); // IMAGE BY ID
-// router.post('/images', null); // IMAGE CREATOR | AUTH ONLY
-// router.post('/images/like/:id', null) // SET LIKE FOR THE IMAGE | AUTH ONLY
+// router.get('/images', null); // FIXME GET LATEST IMAGES BY PAGE 
+// router.get('/images/random', null); // FIXME GET RANDOM IMAGE (FIX)
+// router.get('/images/search', null); // FIXME SEARCH IMAGES BY TAGS OR NAME
+// router.get('/images/:id', null); // FIXME IMAGE BY ID
+// router.post('/images', null); // FIXME IMAGE CREATOR | AUTH ONLY
+// router.post('/images/like/:id', null) // FIXME SET LIKE FOR THE IMAGE | AUTH ONLY
 router.get('/images/user/:id', imageController.userImages); // USER IMAGES BY PAGE
 
-router.get('/user', [authOnlyMiddleware], userController.getInfo); // USER INFO | AUTH ONLY
-// router.get('/user/likes', null); // USER LIKES BY PAGE | AUTH ONLY
+router.get('/user', [authOnlyMiddleware], userController.getInfo); // REVIEW USER INFO | AUTH ONLY
+// router.get('/user/likes', null); // FIXME USER LIKES BY PAGE | AUTH ONLY
 router.post('/user/avatar', [authOnlyMiddleware,
     check('avatar')
         .custom(httpFileExists)
         .custom(httpFileIsImage)
-], userController.uploadAvatar); // UPLOAD USER AVATAR | AUTH ONLY
+], userController.uploadAvatar); // REVIEW UPLOAD USER AVATAR | AUTH ONLY
 
 router.post('/login',
     [body('username').notEmpty(),
     body('password').notEmpty()],
-    authController.login); // USER LOGIN
+    authController.login); // REVIEW USER LOGIN
 
 router.post('/registration',
     [body('username').notEmpty().isLength({ min: process.env.USERNAME_MIN_LENGTH, max: process.env.USERNAME_MAX_LENGTH }),
     body('password').notEmpty().isLength({ min: process.env.PASSWORD_MIN_LENGTH, max: process.env.PASSWORD_MAX_LENGTH })],
-    authController.registration); // USER REGISTRATION
+    authController.registration); // REVIEW USER REGISTRATION
 
-router.get('/refresh', [authOnlyMiddleware], authController.refresh); // REFRESH TOKENS
-router.get('/logout', [authOnlyMiddleware], authController.logout); // LOGOUT
+router.get('/refresh', [authOnlyMiddleware], authController.refresh); // REVIEW REFRESH TOKENS
+router.get('/logout', [authOnlyMiddleware], authController.logout); // REVIEW LOGOUT
 
 export default router;
